00ef174f3149ca16e995dcd5b56bbe0d285e4615
Ingrid Oliveira <ngrid.vianna1@outlook.com>
2025-03-25 16:25:00 -0300

    :tada: first commit

---
+++ src/components/cardNews.js
@@ -0,0 +1,49 @@
+class cardNews extends HTMLElement {
+    constructor(){
+        super();
+
+        const shadow = this.attachShadow({mode: "open"});
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.styles());
+    }
+    
+    build(){
+        // component root
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class", "card");
+
+        // cardLeft
+        const cardLeft = document.createElement("div");
+        cardLeft.setAttribute("class","card__left");
+        
+        // autor - filho do cardLeft
+        const autor = document.createElement("span");
+        cardLeft.appendChild(autor);
+        
+        // linkTitle - filho do cardleft
+        const linkTitle = document.createElement("a");
+        cardLeft.appendChild(linkTitle);
+        
+        // newsContent - filho do cardLeft
+        const newsContent = document.createElement("p");
+        cardLeft.appendChild(newsContent);
+
+        // CardRight
+        const cardRight = document.createElement("div");
+        cardRight.setAttribute("class","card__right");
+        const newsImage = document.createElement("img");
+        cardRight.appendChild(newsImage);
+
+        // Chamada componentRoot para as divs de classe CardLeft e CardRight
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
+
+
+        return componentRoot;
+    }
+
+    styles(){
+
+    }
+}
+customElements.define("card-news", cardNews);
