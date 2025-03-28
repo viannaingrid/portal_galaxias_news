56cbbb63509b48a22fcefa8a43fe331bf499b149
Ingrid Oliveira <ngrid.vianna1@outlook.com>
2025-03-28 14:28:06 -0300

    :construction: finish js

--- src/components/cardNews.js
+++ src/components/cardNews.js
@@ -25,6 +25,7 @@ class cardNews extends HTMLElement {
         const linkTitle = document.createElement("a");
         cardLeft.appendChild(linkTitle);
         linkTitle.textContent = this.getAttribute("title");
+        linkTitle.href = this.getAttribute("link-url");
         
         // newsContent - filho do cardLeft
         const newsContent = document.createElement("p");
@@ -34,7 +35,10 @@ class cardNews extends HTMLElement {
         // CardRight
         const cardRight = document.createElement("div");
         cardRight.setAttribute("class","card__right");
+
         const newsImage = document.createElement("img");
+        newsImage.src = this.getAttribute("photo") || "assets/img/foto-default.jpg";
+        newsImage.alt = "Foto da noticia";
         cardRight.appendChild(newsImage);
 
         // Chamada componentRoot para as divs de classe CardLeft e CardRight
@@ -46,7 +50,58 @@ class cardNews extends HTMLElement {
     }
 
     styles(){
+        const style = document.createElement("style");
+        style.textContent = `
+            .card {
+                width: 90%;
+                -webkit-box-shadow: 9px 9px 19px 1px rgba(76, 74, 74, 0.68);
+                -moz-box-shadow: 9px 9px 19px 1px rgba(85, 84, 84, 0.68);
+                box-shadow: 9px 9px 19px 1px rgba(67, 66, 66, 0.68);
+                display: flex;
+                flex-direction: row;
+                justify-content: space-between;
+                margin-top: 15px
+            }
 
+
+            .card__left{
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                padding-left: 10px;
+
+                h1{
+                    margin-top: 15px;
+                    margin-bottom: 12px;
+                    font-size: 25px;
+                }
+
+                p {
+                    color: #847c7c;
+                }
+
+                span {
+                    font-weight: 400;
+                    color: #847c7c;
+                    margin-bottom: 15px;
+                }
+
+                a{
+                text-decoration: none;
+                color: #000;
+                font-size: 20px;
+                font-weight: bold;
+                }
+            }
+
+            .card__right img {
+                width: 288px;
+                padding: 15px;
+                border-radius: 20px;
+            }
+        `
+
+        return style;
     }
 }
 customElements.define("card-news", cardNews);
